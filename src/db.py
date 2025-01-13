74163eb8cca17f25d9b468a8090a9be3c83e0a26
Daniel A. Fernández <daniel_alejandro_fernandez@hotmail.com>
2025-01-13 00:21:37 -0300

    inicial

--- src/db.py
+++ src/db.py
@@ -1,10 +1,29 @@
+import mysql.connector.pooling
+
+from config import Config
+
+config = {
+    'user': Config.DB_USER,
+    'database': Config.DB_NAME,
+    'password': Config.DB_PASSWORD
+    }
+
 class Database:
-    def __init__(self, config):
+    def __init__(self):
         """Initialize the connection pool."""
-        pass
+        self.pool = mysql.connector.pooling.MySQLConnectionPool(autocommit=True,              # D= para no tener q hacer commit :/
+                                                                pool_name="pileta",
+                                                                pool_size=5,
+                                                                **config)
 
     def get_connection(self):
-        pass
-    
-    def close_connection(self, connection):
-        pass
+        try:
+            conn = self.pool.get_connection()
+        except mysql.connector.PoolError as err:
+            # logger.exception("Pool agotada D=", err.msg, err.errno)
+            raise
+        except Exception as err:
+            # logger.exception("Error inesperado", err)
+            raise
+        else:
+            return conn
