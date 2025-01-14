da29d1e400ea25bc6857e3fb976f409b7d1d119b
Luis-AP <lap18958@gmail.com>
2025-01-13 23:44:12 -0300

    Default params -> **kwargs

--- src/models/user.py
+++ src/models/user.py
@@ -1,72 +1,34 @@
 class User:
-
-    def __init__(
-        self,
-        user_id=None,
-        email=None,
-        password=None,
-        first_name=None,
-        last_name=None,
-        created_at=None,
-    ):
-        self.id = user_id
-        self.email = email
-        self.password = password
-        self.first_name = first_name
-        self.last_name = last_name
-        self.created_at = created_at
+    def __init__(self, **kwargs):
+        self.id = kwargs.get("id")
+        self.email = kwargs.get("email")
+        self.password = kwargs.get("password")
+        self.first_name = kwargs.get("first_name")
+        self.last_name = kwargs.get("last_name")
+        self.created_at = kwargs.get("created_at")
 
     def __repr__(self):
         return f"<User {self.email}>"
 
 
 class Student(User):
-    def __init__(
-        self,
-        user_id=None,
-        email=None,
-        password=None,
-        first_name=None,
-        last_name=None,
-        created_at=None,
-        student_id=None,
-        enrollment_number=None,
-        major=None,
-        enrolled_at=None,
-    ):
-        super().__init__(
-            user_id, email, password, first_name, last_name, created_at
-        )
-        self.id = student_id
-        self.user_id = user_id
-        self.enrollment_number = enrollment_number
-        self.major = major
-        self.enrolled_at = enrolled_at
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.user_id = kwargs.get("user_id")
+        self.enrollment_number = kwargs.get("enrollment_number")
+        self.major = kwargs.get("major")
+        self.enrolled_at = kwargs.get("enrolled_at")
 
     def __repr__(self):
         return f"<Student {self.email}>"
 
 
 class Professor(User):
-    def __init__(
-        self,
-        user_id=None,
-        email=None,
-        password=None,
-        first_name=None,
-        last_name=None,
-        created_at=None,
-        professor_id=None,
-        department=None,
-        speciality=None,
-    ):
-        super().__init__(
-            user_id, email, password, first_name, last_name, created_at
-        )
-        self.id = professor_id
-        self.user_id = user_id
-        self.department = department
-        self.speciality = speciality
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.user_id = kwargs.get("user_id")
+        self.department = kwargs.get("department")
+        self.specialty = kwargs.get("specialty")
 
     def __repr__(self):
         return f"<Professor {self.email}>"
